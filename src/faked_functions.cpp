da8aad15455034904e3bf1019b7a89899e2c9ce8
Homura <egzozu.be1.bas@gmail.com>
2025-10-12 00:01:48 +0300

    Update faked_functions.cpp

--- src/faked_functions.cpp
+++ src/faked_functions.cpp
@@ -34,10 +34,25 @@ extern "C" {
 
     int cacheflush(long start, long end, long flags) {
         (void)flags;
+    
+    #if defined(__has_builtin)
+    #if __has_builtin(__builtin___clear_cache)
+        __builtin___clear_cache(reinterpret_cast<char*>(start),
+                                reinterpret_cast<char*>(end));
+    #else
+        (void)start; 
+        (void)end;
+    #endif
+    #else
     #if defined(__GNUC__)
         __builtin___clear_cache(reinterpret_cast<char*>(start),
                                 reinterpret_cast<char*>(end));
+    #else
+        (void)start;
+        (void)end;
     #endif
+    #endif
+    
         return 0;
     }
 }
