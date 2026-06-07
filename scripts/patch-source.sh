b2b0bd124ee7988e3307ae089b4cb0f830f6a8fa
Homura <egzozu.be.bas@gmail.com>
2026-06-07 16:28:21 +0300

    asdasdasdasdasasdasd

--- scripts/patch-source.sh
+++ scripts/patch-source.sh
@@ -255,6 +255,7 @@ sed -i 's/extra_args\.flavor = \&flavor;/extra_args.flavor = (uint32_t *)\&flavo
 # bionic: some functions are is introduced in API 29 and upper but users might target
 # lower APIs which some symbols won't be available. Guard the uses in files under an API check.
 sed -i 's/#if defined(__BIONIC__)/#if defined(__BIONIC__) \&\& __ANDROID_API__ >= 29/g' ${PWD_SRC}/src/libbase/include/android-base/unique_fd.h
+sed -i 's/__INTRODUCED_IN([0-9]*)//g' ${PWD_SRC}/src/logging/liblog/include/android/log.h
 
 # brotli: restore static-library support
 ( cd ${PWD_SRC}/src/brotli && git apply ../../patches/0001-add-static-support-back-to-brotli.patch )
