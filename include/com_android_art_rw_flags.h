be773693e329aed4177d9f2d3d25deea938a8c9d
Homura <egzozu.be.bas@gmail.com>
2026-06-25 10:00:52 +0300

    update shims

--- include/com_android_art_rw_flags.h
+++ include/com_android_art_rw_flags.h
@@ -8,6 +8,14 @@ static inline bool com_android_art_rw_flags_test_rw_flag() {
     return false;
 }
 
+static inline bool com_android_art_rw_flags_read_only_dynamic_code_load_ww_log() {
+    return false;
+}
+
+static inline bool com_android_art_rw_flags_read_only_dynamic_code_load_throw_exception() {
+    return false;
+}
+
 #ifdef __cplusplus
 }
 #endif
